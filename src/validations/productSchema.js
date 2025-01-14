d2f9d7e90c1819a2dd8a5a5158caa47b7aa2b51b
tahayk3 <tahayk3@gmail.com>
2025-01-14 13:12:40 -0600

    validacion

--- src/validations/productSchema.js
+++ src/validations/productSchema.js
@@ -4,7 +4,7 @@ export const productSchema = yup.object().shape({
   nombre: yup.string().required("El nombre del producto es obligatorio"),
   marca: yup.string().required("La marca es obligatoria"),
   codigo: yup.string().required("El código es obligatorio"),
-  stock: yup.number().positive("El stock debe ser positivo").integer("Debe ser un número entero").required("El stock es obligatorio"),
+  stock: yup.number().min(0, "El stock no puede ser menor a 0").integer("Debe ser un número entero").required("El stock es obligatorio"),
   precio: yup.number().positive("El precio debe ser positivo").required("El precio es obligatorio"),
   id_categoria_producto: yup.number().required("El ID de categoría es obligatorio"),
   id_estado: yup.number().required("El ID de estado es obligatorio"),
